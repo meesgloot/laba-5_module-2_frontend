ba9d853779870938fc024d5fec7657771341a312
Анна Шмакова <118816204+meesgloot@users.noreply.github.com>
2023-08-25 15:11:34 +0300

    Create script5.js

---
+++ js/script5.js
@@ -0,0 +1,31 @@
+function show1() {
+	document.getElementById('benz').className='show';
+}
+function hide1() {
+	document.getElementById('benz').className='hide';
+}
+
+function show2() {
+	document.getElementById('ataca').className='show';
+}
+function hide2() {
+	document.getElementById('ataca').className='hide';
+}
+
+function show3() {
+	document.getElementById('krd').className='show';
+}
+function hide3() {
+	document.getElementById('krd').className='hide';
+}
+
+function show4() {
+	document.getElementById('dc').className='show';
+}
+function hide4() {
+	document.getElementById('dc').className='hide';
+}
+
+function image(persona) {
+	document.getElementById("persona").src= persona + ".jpg"; 
+}
